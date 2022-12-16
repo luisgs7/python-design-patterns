725efafc00a4e62bb019013340e87da8184c47e7
luisgs7 <luisgsilva270@gmail.com>
2022-12-16 16:47:46 -0300

    simple factory 01

--- creational/factories/simple_factory/simple_factory_1.py
+++ creational/factories/simple_factory/simple_factory_1.py
@@ -17,3 +17,47 @@ Simple Factory <- Uma espécie de Factory Method parametrizado
 Simple Factory pode não ser considerado um padrão de projeto por si só
 Simple Factory pode quebrar princípios do SOLID
 """
+from random import choice
+from abc import ABC, abstractmethod
+
+
+class Veiculo(ABC):
+    @abstractmethod
+    def buscar_cliente(self) -> None:
+        pass
+
+
+class CarroLuxo(Veiculo):
+    def buscar_cliente(self) -> None:
+        print("Carro de luxo está buscando o cliente...")
+
+
+class CarroPopular(Veiculo):
+    def buscar_cliente(self) -> None:
+        print("Carro popular está buscando o cliente...")
+
+
+class Moto(Veiculo):
+    def buscar_cliente(self) -> None:
+        print("Moto está buscando o cliente...")
+
+
+class VeiculoFactory:
+    @staticmethod
+    def get_carro(tipo: str) -> Veiculo:
+        if tipo == "luxo":
+            return CarroLuxo()
+        elif tipo == "popular":
+            return CarroPopular()
+        elif tipo == "moto":
+            return Moto()
+        else:
+            return "Veiculo não existe."
+
+
+if __name__ == "__main__":
+    carros_disponiveis = ["luxo", "popular", "moto"]
+
+    for i in range(10):
+        carro = VeiculoFactory.get_carro(choice(carros_disponiveis))
+        carro.buscar_cliente()
